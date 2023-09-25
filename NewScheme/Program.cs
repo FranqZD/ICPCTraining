a3f69201ff06769684cc90a0dc60bd5aae2b799a
FranqZD <a19300095@ceti.mx>
2023-09-25 09:27:48 -0600

    Resolved problem NewScheme

--- NewScheme/Program.cs
+++ NewScheme/Program.cs
@@ -6,7 +6,27 @@ class Program
 {
 static void Main (string[] args)
 {
-    
+    string[] numerosS = new string[8];
+    int[] numerosI = new int[8];
+    int l=0;
+    for(int i = 0;i<=7;i++){
+    if(i==0){
+        numerosS=ReadLine().Split(' ');
+        foreach(string s in numerosS){
+            numerosI[l]=int.Parse(s);
+            l++;
+        }
+    }
+    else{
+        if(numerosI[i-1]<=numerosI[i] && numerosI[i]>=100 && numerosI[i]<=675 && numerosI[i]%25==0){
+
+        }else{
+            WriteLine("No");
+            return;
+        }
+    }
+    }
+    WriteLine("Yes");
 }
 }
 }
