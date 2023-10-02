80179756967201b0d36e8f7a2db986daa4c4333b
FranqZD <a19300095@ceti.mx>
2023-10-01 22:24:35 -0600

    More and more

---
+++ RemainingTime/Main.py
@@ -0,0 +1,8 @@
+Data = input().split()
+Hini = int(Data[0])
+Hsuma = int(Data[1])
+Htotal = Hsuma + Hini
+if(Hsuma+Hini >= 24):
+    Htotal = Htotal-24
+print(Htotal)
+    
---
+++ Restaurant/Main.py
@@ -0,0 +1,4 @@
+N = int(input())
+Resta =int(N/15)
+N = (N*800) - (Resta*200)
+print(N)
