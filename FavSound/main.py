6d2be4d4e07d78df950f15b348bd1e5e10245e66
FranqZD <a19300095@ceti.mx>
2023-10-03 13:33:43 -0600

    More and more

--- FavSound/main.py
+++ FavSound/main.py
@@ -1,6 +1,7 @@
-Data = input()
-Datos = [int(datos) for datos in Data.split()]
-if(Datos[0]*Datos[2]<= Datos[1]):
-    print(Datos[2])
-else:
-    print(int(Datos[1]/Datos[0]))
+Ciclos = input()
+for cadena in range(Ciclos+1):
+    Data = input()
+    if(Datos[0]*Datos[2]<= Datos[1]):
+        print(Datos[2])
+    else:
+        print(int(Datos[1]/Datos[0]))
